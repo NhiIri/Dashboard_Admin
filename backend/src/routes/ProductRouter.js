efa1590dfc9e1ff1eb9b32d050cae1157aebdb3e
HNNhi <huynhnguyen190109@gmail.com>
2024-03-24 15:12:08 +0700

    update

--- backend/src/routes/ProductRouter.js
+++ backend/src/routes/ProductRouter.js
@@ -10,7 +10,6 @@ router.delete('/delete/:id', authMiddleWare, ProductController.deleteProduct)
 router.get('/get-all', ProductController.getAllProduct)
 router.post('/delete-many', authMiddleWare, ProductController.deleteMany)
 router.get('/get-all-type', ProductController.getAllType)
-// router.get('/getproduct-category/:categoryName', ProductController.getProductsByCategory);
 router.get('/category/:categoryId', ProductController.getProductsByCategory);
-
+// router.get('/getproduct-category/:categoryName', ProductController.getProductsByCategory);
 module.exports = router
